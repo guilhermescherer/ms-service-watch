9fd4ccc89cd5a5d80010bd0869198436d2dc621b
guilherme <guilherme_scherer@outlook.com>
2025-07-20 15:57:21 -0300

    feat: add sonar issue for testing

--- src/test/java/com/guilhermescherer/msservicewatch/controller/ServiceEndpointControllerIT.java
+++ src/test/java/com/guilhermescherer/msservicewatch/controller/ServiceEndpointControllerIT.java
@@ -30,7 +30,7 @@ import static org.hamcrest.Matchers.nullValue;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-class ServiceEndpointControllerIT {
+public class ServiceEndpointControllerIT {
 
     private static final String VALIDATION_ERROR = "Validation error";
     private static final String NOT_FOUND_ENTITY_ERROR = "Not found entity";
