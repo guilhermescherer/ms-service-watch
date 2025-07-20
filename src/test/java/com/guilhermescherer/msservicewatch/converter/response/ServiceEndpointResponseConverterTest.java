691360a217050a58c185a2af9fb748eaa5f331bb
guilherme <guilherme_scherer@outlook.com>
2025-07-20 16:37:18 -0300

    feat: resolve sonar issues

--- src/test/java/com/guilhermescherer/msservicewatch/converter/response/ServiceEndpointResponseConverterTest.java
+++ src/test/java/com/guilhermescherer/msservicewatch/converter/response/ServiceEndpointResponseConverterTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ServiceEndpointResponseConverterTest {
+class ServiceEndpointResponseConverterTest {
 
     @InjectMocks
     private ServiceEndpointResponseConverter converter;
