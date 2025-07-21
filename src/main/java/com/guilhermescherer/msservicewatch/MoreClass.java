cb49ab1c5add2227ebcf04a5feb2771a2de4a249
guilherme <guilherme_scherer@outlook.com>
2025-07-21 20:03:50 -0300

    feat: remove issue sonar class

--- src/main/java/com/guilhermescherer/msservicewatch/MoreClass.java
+++
@@ -1,9 +0,0 @@
-package com.guilhermescherer.msservicewatch;
-
-public class MoreClass {
-
-    public static void main(String[] args) {
-        String foo = null;
-        System.out.println(foo.toString());
-    }
-}
